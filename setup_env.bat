f726660935bb22f052ce243af63ce5da58a50101
rahulchowdary143 <yugeshyogi972@gmail.com>
2026-01-29 15:56:03 +0530

    makde changes in setup_env.bat file

--- setup_env.bat
+++ setup_env.bat
@@ -1,4 +1,7 @@
 npm install
 npm install -D allure-playwright
 npx playwright install
+npm install -g allure-commandline --force
+npm run "%script%"
+allure generate allure-results -o allure-report --clean
 
