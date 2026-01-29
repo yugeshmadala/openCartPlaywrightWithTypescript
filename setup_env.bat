6de69136450c39d51d1330746c09fe0eef609d13
rahulchowdary143 <yugeshyogi972@gmail.com>
2026-01-29 20:09:07 +0530

    updated .bat file with npm commands

--- setup_env.bat
+++ setup_env.bat
@@ -1,7 +1,5 @@
 npm install
 npm install -D allure-playwright
-npx playwright install
 npm install -g allure-commandline --force
-npm run "%script%"
-allure generate allure-results -o allure-report --clean
+npx playwright install
 
